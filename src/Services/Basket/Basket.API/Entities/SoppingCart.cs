0f6845eeaa0bea55e35a39f6112a4904b7fb0023
Aykhan <Aykhan.Alifov@atltech.com>
2022-02-23 14:51:33 +0400

    Add entities
    ShopingCart
    ShopingCartItem

---
+++ src/Services/Basket/Basket.API/Entities/SoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class SoppingCart
+    {
+        public string Username { get; set; }
+        public List<ShopingCartItem> Items { get; set; } = new List<ShopingCartItem>();
+
+        public SoppingCart()
+        {                            
+        }
+
+        public SoppingCart(string username)
+        {
+            Username = username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = 0;
+                foreach (var item in Items)
+                {
+                    total += item.Price * item.Quantity;
+                }
+
+                return total;
+            }
+        }
+    }
+}
