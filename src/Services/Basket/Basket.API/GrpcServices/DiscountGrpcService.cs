acedaae752859bec0ae1adbe9a2dd38d89098f05
Aykhan <Aykhan.Alifov@atltech.com>
2022-03-05 22:33:37 +0400

    Add DiscountGrpcService

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,22 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _client;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            return await _client.GetDiscountAsync(discountRequest);
+        }
+    }
+}
